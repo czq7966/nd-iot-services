f40299a20352361712f17904956fa5c97182d72a
陈志谦 <761208@nd.com>
2022-05-31 18:29:20 +0800

    improve start.js

--- docker/readme.txt
+++ docker/readme.txt
@@ -1,5 +1,5 @@
 服务 docker 镜像 生成命令：
-docker build -t registry.101.com/60b603a0d9419c00107e378d/nd-iot-services-amd64:0.0.2 .
+docker build -t registry.101.com/60b603a0d9419c00107e378d/nd-iot-services-amd64:0.0.3 .
 
 docker build -t registry.101.com/60b603a0d9419c00107e378d/nd-iot-services-armv7:0.0.2 .
 
@@ -99,4 +99,4 @@ registry.101.com/60b603a0d9419c00107e378d/nd-iot-services:0.0.2 \
 npm start
 
 
-docker run -d -e IOT_CONFIG_FILE="/services/nd-iot-services/docker/config.json" -e IOT_CODE_BRANCH=dev -e IOT_ENABLE_AUTO_UPDATE=1 -p 11880:11880 -p 11881:11881 -p 11882:11882 -p 11883:11883 -p 18090:18090 -p 18091:18091 -p 18092:18092 -p 18093:18093 -p 18094:18094 -p 18095:18095 -p 18096:18096 -e IOT_APP_ID=ndiot -e IOT_ENABLE_MQTT=1 -e IOT_ENABLE_DIO=1 -e IOT_ENABLE_DSP=1 -e IOT_ENABLE_EDG=1 registry.101.com/60b603a0d9419c00107e378d/nd-iot-services:0.0.2 npm start
+docker run -d -e IOT_CONFIG_FILE="/services/nd-iot-services/docker/config.json" -e IOT_CODE_BRANCH=dev -e IOT_ENABLE_AUTO_UPDATE=1 -p 8080:8080 -p 11881:11881 -p 11882:11882 -p 11883:11883 -p 18090:18090 -p 18091:18091 -p 18092:18092 -p 18093:18093 -p 18094:18094 -p 18095:18095 -p 18096:18096 -e IOT_APP_ID=ndiot -e IOT_ENABLE_MQTT=1 -e IOT_ENABLE_DIO=1 -e IOT_ENABLE_DSP=1 -e IOT_ENABLE_EDG=1 registry.101.com/60b603a0d9419c00107e378d/nd-iot-services-amd64:0.0.3 npm start
--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const path = require("path");
 let exec = require("child_process").exec;
 let execSync = require("child_process").execSync;
 let spawn = require("child_process").spawn;
-let useSpawn = false;
+let useSpawn = true;
 
 let isWin32 = () => {
     return process.platform == "win32";
@@ -39,14 +39,19 @@ let updateServices = (sync) => {
 
 //本地MQTT服务
 let startMQTT = () => {
-    let cmd;
-    if (isWin32()) 
-        cmd = "npm run nd:mqtt:start";       
-    else 
-        cmd = "npm run nd:mqtt:service:start";
     if (useSpawn) {
-        spawn(cmd);
+        let cmd = "node"; 
+        let args = ["./src/index.js"];
+        let cwd = path.resolve(__dirname, "../", "nd-iot-mqtt-broker");
+        let child = spawn(cmd, args, { stdio: 'inherit', cwd: cwd});         
+        child.on("close", (code, signal) => {
+                console.error("mqtt closed");
+                setTimeout(() => {
+                        startMQTT();               
+                }, 1000);
+        });
     }else {
+        let cmd = "npm run nd:mqtt:start"; 
         exec(cmd, function(error, stdout, stderr) {
             if (error || stderr)
                 console.error("mqtt service error: %s , %s!", error, stderr);
@@ -55,19 +60,28 @@ let startMQTT = () => {
                 console.error("mqtt service stopped");
         });  
     }
+    console.log("mqtt started");
 }
 
 //数据服务
-let startDIO = () => {    
-    let cmd;
-    if (isWin32()) 
-        cmd = "npm run nd:dataio:start";               
-    else 
-        cmd = "npm run nd:dataio:service:start";
-
+let startDIO = () => {  
     if (useSpawn) {
-        spawn(cmd);
+        let cmd = "node"; 
+        let args = ["./index.js", "-u", "./data/dataio/nd"];
+        let cwd = path.resolve(__dirname);
+
+        let child = spawn(cmd, args, { stdio: 'inherit', cwd: cwd});    
+        child.on("error", (code) => {
+            console.error("dataio error: " + code);
+        })   
+        child.on("close", (code, signal) => {
+                console.error("dataio closed");
+                setTimeout(() => {
+                    startDIO();               
+                }, 1000);
+        });
     }else {
+        let cmd = "npm run nd:dataio:start"; 
         exec(cmd, function(error, stdout, stderr) {
             if (error || stderr)
                 console.error("dataio service error: %s , %s!", error, stderr);
@@ -76,19 +90,28 @@ let startDIO = () => {
                 console.error("dataio service stopped");
         });
     }
+    console.log("dataio started");
 }
 
 //调度服务
 let startDSP = () => {    
-    let cmd;
-    if (isWin32()) 
-        cmd = "npm run nd:dispatcher:start";               
-    else 
-        cmd = "npm run nd:dispatcher:service:start";
-
     if (useSpawn) {
-        spawn(cmd);
+        let cmd = "node"; 
+        let args = ["./index.js", "-u", "./data/dispatcher/nd"];
+        let cwd = path.resolve(__dirname);
+
+        let child = spawn(cmd, args, { stdio: 'inherit', cwd: cwd});    
+        child.on("error", (code) => {
+            console.error("dispatcher error: " + code);
+        })     
+        child.on("close", (code, signal) => {
+                console.error("dispatcher closed");
+                setTimeout(() => {
+                    startDSP();               
+                }, 1000);
+        });
     }else {
+        let cmd = "npm run nd:dispatcher:start";
         exec(cmd, function(error, stdout, stderr) {
             if (error || stderr)
                 console.error("dispatcher service error: %s , %s!", error, stderr);
@@ -97,18 +120,28 @@ let startDSP = () => {
                 console.error("dispatcher service stopped");
         });
     }
+    console.log("dispatcher started");
 }
 
 //边缘服务
 let startEDG = () => {    
-    let cmd;
-    if (isWin32()) 
-        cmd = "npm run nd:edge:start";               
-    else 
-        cmd = "npm run nd:edge:service:start";
     if (useSpawn) {
-        spawn(cmd);
+        let cmd = "node"; 
+        let args = ["./index.js", "-u", "./data/edge/nd"];
+        let cwd = path.resolve(__dirname);
+
+        let child = spawn(cmd, args, { stdio: 'inherit', cwd: cwd});    
+        child.on("error", (code) => {
+            console.error("edge error: " + code);
+        })      
+        child.on("close", (code, signal) => {
+                console.error("edge closed");
+                setTimeout(() => {
+                    startEDG();               
+                }, 1000);
+        });
     }else {
+        let cmd = "npm run nd:edge:start"; 
         exec(cmd, function(error, stdout, stderr) {
             if (error || stderr)
                 console.error("edge service error: %s , %s!", error, stderr);
@@ -117,41 +150,81 @@ let startEDG = () => {
                 console.error("edge service stopped");
         });
     }
+    console.log("edge started");
 }
 
 //ND平台对接服务
 let startPLF_ND = () => {    
-    let cmd;
-    if (isWin32()) 
-        cmd = "npm run nd:platform:nd:start";               
-    else 
-        cmd = "npm run nd:platform:nd:service:start";
+    if (useSpawn) {
+        let cmd = "node"; 
+        let args = ["./index.js", "-u", "./data/platform/nd"];
+        let cwd = path.resolve(__dirname);
 
-    exec(cmd, function(error, stdout, stderr) {
-        if (error || stderr)
-            console.error("platform nd service error: %s , %s!", error, stderr);
+        let child = spawn(cmd, args, { stdio: 'inherit', cwd: cwd});    
+        child.on("error", (code) => {
+            console.error("edge error: " + code);
+        })     
+        child.on("close", (code, signal) => {
+                console.error("platform nd closed");
+                setTimeout(() => {
+                    startPLF_ND();               
+                }, 1000);
+        });
+    }else {
+        let cmd = "npm run nd:platform:nd:start";
+        exec(cmd, function(error, stdout, stderr) {
+            if (error || stderr)
+                console.error("platform nd service error: %s , %s!", error, stderr);
 
-        if (isWin32()) 
-            console.error("platform nd service stopped");
-    });
+            if (isWin32()) 
+                console.error("platform nd service stopped");
+        });
+    }
+    console.log("platform nd started");
 }
 
 //巴法云平台对接服务
 let startPLF_BFY = () => {    
-    let cmd;
-    if (isWin32()) 
-        cmd = "npm run nd:platform:bfy:start";               
-    else 
-        cmd = "npm run nd:platform:bfy:service:start";
+    if (useSpawn) {
+        let cmd = "node"; 
+        let args = ["./index.js", "-u", "./data/platform/bfy"];
+        let cwd = path.resolve(__dirname);
 
-    exec(cmd, function(error, stdout, stderr) {
-        if (error || stderr)
-            console.error("platform bfy service error: %s , %s!", error, stderr);
+        let child = spawn(cmd, args, { stdio: 'inherit', cwd: cwd});    
+        child.on("error", (code) => {
+            console.error("edge error: " + code);
+        })       
+        child.on("close", (code, signal) => {
+                console.error("platform bfy closed");
+                setTimeout(() => {
+                    startPLF_BFY();               
+                }, 1000);
+        });
+    }else {
+        let cmd = "npm run nd:platform:bfy:start";
+        exec(cmd, function(error, stdout, stderr) {
+            if (error || stderr)
+                console.error("platform bfy service error: %s , %s!", error, stderr);
 
-        if (isWin32()) 
-            console.error("platform bfy service stopped");
-    });
+            if (isWin32()) 
+                console.error("platform bfy service stopped");
+        });
+    }
+    console.log("platform bfy started");
 }
+
+
+let testZ2M = (datadir) => {
+    process.env.ZIGBEE2MQTT_DATA = datadir;
+    let cmd = "node"; 
+    let args = ["../zigbee2mqtt/index.js"];
+    // let cwd = path.resolve(__dirname, "../", "zigbee2mqtt");
+    let child = spawn(cmd, args, { stdio: 'inherit'});         
+    child.on("close", (code, signal) => {
+            console.error("zigbee2mqtt closed");
+
+    });    
+};
 
 if (process.env.IOT_ENABLE_AUTO_UPDATE == "1") {
     updateZ2M(true);
@@ -182,10 +255,12 @@ if (process.env.IOT_ENABLE_PLF_BFY == "1") {
     startPLF_BFY();
 } 
 
-
+// testZ2M("E:/iotdata/zigbee2mqtt/data/ESP8266x00c17ca1");
+// process.env.ZIGBEE2MQTT_DATA = "";
+// testZ2M("E:/iotdata/zigbee2mqtt/data/ESP8266x0006cf1d");
 
 
 require("./src/http-server.js");
 
 
-console.log(process.env);
+// console.log(process.env);
